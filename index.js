34dde3802990e942127d0cc592c2552352e3ea61
thefidanabdulla <thefidanabdulla@gmail.com>
2024-05-11 20:11:15 +0400

    update api

--- index.js
+++ index.js
@@ -5,17 +5,9 @@ const app = express();
 
 app.use(express.json());
 
+// get
 app.get("/", (req, res) => {
   res.send("salam from node api saaaalam");
-});
-
-app.post("/api/products", async (req, res) => {
-  try{
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  }catch(error){
-    res.status(500).json({ message: error.message });
-  }
 });
 
 app.get('/api/products', async (req, res) => {
@@ -27,7 +19,7 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-app.get('/api/products/:id', async (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
@@ -36,6 +28,35 @@ app.get('/api/products/:id', async (req, res) => {
     res.status(500).json({message: error.message});
   }
 });
+
+// create
+app.post("/api/products", async (req, res) => {
+  try{
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  }catch(error){
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// update
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if(!product){
+      return res.status(404).json({ message: "Product not found :/" })
+    }
+    
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+
 
 mongoose.connect("mongodb+srv://thefidanabdulla:luGkULvJG8DI0QaK@backenddb.fkpkffs.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
 .then(() => {
